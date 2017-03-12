e65224f075eb179a2ecbf3937002d8f1a5f01a0c
a.alekseev10 <a.alekseev10@tinkoff.ru>
2017-03-12 21:24:31 +0300

    2

--- app.js
+++ app.js
@@ -13,21 +13,43 @@ var itemElementList = listElement.children;
 // itemElementList = document.querySelectorAll('.list__item');
 // itemElementList = document.querySelectorAll('.list .list__item');
 
-var todo = 'Добавить тудушку в список';
+var todoList = [
+    'Позвонить в сервис',
+    'Купить хлеб',
+    'Захватить мир',
+    'Добавить тудушку в список'
+];
 
-var newItemElement = document.createElement('li');
-newItemElement.classList.add('list__item', 'task', 'task_todo');
-// newItemElement.innerHTML = todo;
-// newItemElement.textContent = todo;
 
-var htmlToAdd = '<div class="task__status task__status_todo"></div>' +
-    '<span class="task__name">' + todo + '</span>' +
-    '<div class="task__delete-button">❌</div>';
+// функция по генерации элементов
+function addTodo(name) {
+    var htmlToAdd = '<div class="task__status task__status_todo"></div>' +
+        '<span class="task__name">' + name + '</span>' +
+        '<div class="task__delete-button">❌</div>';
 
-newItemElement.innerHTML = htmlToAdd;
+    var newItemElement = document.createElement('li');
+    newItemElement.classList.add('list__item', 'task', 'task_todo');
+    newItemElement.innerHTML = htmlToAdd;
+    return newItemElement;
+}
 
-// / listElement.appendChild(newItemElement);
-// console.log(listElement.children);
+todoList
+    .map(addTodo)
+    .forEach(function (element) {
+        listElement.appendChild(element);
+    });
 
-listElement.insertBefore(newItemElement, listElement.firstChild);
-console.log(listElement.children);
+var templateElement = document.getElementById('todoTemplate');
+var templateContainer = 'content' in templateElement ? templateElement.content : templateElement;
+
+function addTodoFromTemplate(name) {
+    var newElement = templateContainer.querySelector('.task').cloneNode(true);
+    newElement.querySelector('.task__name').textContent = name;
+    return newElement;
+}
+
+todoList
+    .map(addTodoFromTemplate)
+    .forEach(function (element) {
+        listElement.appendChild(element);
+    });
